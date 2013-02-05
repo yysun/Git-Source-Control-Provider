8cd867ebfd5ab253d83b923158bbb4321bc50f9d
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-04 20:55:56 -0600

    Add DiffUpdateBackgroundParser (not yet used)

---
+++ Diff/DiffParseResultEventArgs.cs
@@ -0,0 +1,25 @@
+﻿namespace GitScc.Diff
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.Text;
+
+    public class DiffParseResultEventArgs : ParseResultEventArgs
+    {
+        private readonly List<HunkRangeInfo> _diff;
+
+        public DiffParseResultEventArgs(ITextSnapshot snapshot, TimeSpan elapsedTime, List<HunkRangeInfo> diff)
+            : base(snapshot, elapsedTime)
+        {
+            _diff = diff;
+        }
+
+        public List<HunkRangeInfo> Diff
+        {
+            get
+            {
+                return _diff;
+            }
+        }
+    }
+}

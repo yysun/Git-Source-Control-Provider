e660278dd171337a95839cb2d0048c2587558f5d
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-05 20:53:59 -0600

    Properly use Git's core.autocrlf configuration setting for diffs

--- Diff/GitCommands.cs
+++ Diff/GitCommands.cs
@@ -1,12 +1,19 @@
 ﻿namespace GitScc.Diff
 {
     using System.Collections.Generic;
-    using NGit.Diff;
     using Buffer = System.Buffer;
     using Constants = NGit.Constants;
+    using CoreConfig = NGit.CoreConfig;
+    using Edit = NGit.Diff.Edit;
+    using EditList = NGit.Diff.EditList;
+    using Environment = System.Environment;
+    using HistogramDiff = NGit.Diff.HistogramDiff;
     using ITextDocument = Microsoft.VisualStudio.Text.ITextDocument;
     using ITextSnapshot = Microsoft.VisualStudio.Text.ITextSnapshot;
     using Path = System.IO.Path;
+    using RawText = NGit.Diff.RawText;
+    using RawTextComparator = NGit.Diff.RawTextComparator;
+    using WorkingTreeOptions = NGit.Treewalk.WorkingTreeOptions;
 
     public class GitCommands : IGitCommands
     {
@@ -35,11 +42,45 @@
                 content = completeContent;
             }
 
+            byte[] previousContent = GetPreviousRevision(tracker, fileName);
             RawText b = new RawText(content);
-            RawText a = new RawText(tracker.GetFileContent(fileName) ?? new byte[0]);
-            EditList edits = diff.Diff(RawTextComparator.WS_IGNORE_TRAILING, a, b);
+            RawText a = new RawText(previousContent);
+            EditList edits = diff.Diff(RawTextComparator.DEFAULT, a, b);
             foreach (Edit edit in edits)
                 yield return new HunkRangeInfo(edit, a, b);
+        }
+
+        private static byte[] GetPreviousRevision(GitFileStatusTracker tracker, string fileName)
+        {
+            byte[] cachedBytes = tracker.GetFileContent(fileName);
+            if (cachedBytes == null)
+                return cachedBytes;
+
+            if (Environment.NewLine != "\r\n")
+                return cachedBytes;
+
+            WorkingTreeOptions options = tracker.Repository.GetConfig().Get(WorkingTreeOptions.KEY);
+            if (options.GetAutoCRLF() != CoreConfig.AutoCRLF.TRUE)
+                return cachedBytes;
+
+            int lines = 0;
+            for (int i = 0; i < cachedBytes.Length; i++)
+            {
+                if (cachedBytes[i] == '\n' && (i == 0 || cachedBytes[i - 1] != '\r'))
+                    lines++;
+            }
+
+            byte[] result = new byte[cachedBytes.Length + lines];
+            for (int i = 0, j = 0; i < cachedBytes.Length; i++)
+            {
+                byte current = cachedBytes[i];
+                if (current == '\n' && (i == 0 || cachedBytes[i - 1] != '\r'))
+                    result[j++] = (byte)'\r';
+
+                result[j++] = current;
+            }
+
+            return result;
         }
     }
 }

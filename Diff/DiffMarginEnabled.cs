e37e0b0d66be1e34feb934cc932d84aa9dc7b2c8
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-07 09:11:35 -0600

    Add and export a ViewOptionDefinition for controlling visibility of the diff margin

---
+++ Diff/DiffMarginEnabled.cs
@@ -0,0 +1,27 @@
+﻿namespace GitScc.Diff
+{
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(EditorOptionDefinition))]
+    [Name(GitTextViewOptions.DiffMarginName)]
+    public sealed class DiffMarginEnabled : ViewOptionDefinition<bool>
+    {
+        public override bool Default
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override EditorOptionKey<bool> Key
+        {
+            get
+            {
+                return GitTextViewOptions.DiffMarginId;
+            }
+        }
+    }
+}

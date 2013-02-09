d45fa42de7e522eb2b949679f11c1538e7e10210
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-09 15:49:52 -0600

    Current branch is shown in Pending Changes window caption, no need to duplicate right below it

--- PendingChangesView.xaml.cs
+++ PendingChangesView.xaml.cs
@@ -313,7 +313,7 @@ namespace GitScc
                         ShowStatusMessage("");
 
                         var changed = changedFiles;
-                        this.label3.Content = string.Format("Changed files: ({0}) +{1} ~{2} -{3} !{4}", tracker.CurrentBranch,
+                        this.label3.Content = string.Format("Changed files (+{0} ~{1} -{2} !{3})",
                             changed.Where(f => f.Status == GitFileStatus.New || f.Status == GitFileStatus.Added).Count(),
                             changed.Where(f => f.Status == GitFileStatus.Modified || f.Status == GitFileStatus.Staged).Count(),
                             changed.Where(f => f.Status == GitFileStatus.Deleted || f.Status == GitFileStatus.Removed).Count(),

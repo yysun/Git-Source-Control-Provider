77ac470d61bd0bd16e3b6f62784cd9e729566931
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-10 17:20:43 -0600

    Add wrapper classes to access Visual Studio 2012 brush keys without breaking the 2010 build (2010 fallback colors are not set yet)

---
+++ PlatformUI/HeaderColors.cs
@@ -0,0 +1,103 @@
+﻿namespace GitScc.PlatformUI
+{
+    using System.Diagnostics;
+    using Microsoft.VisualStudio.Shell;
+
+    public static class HeaderColors
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object SeparatorLineBrushKey
+        {
+            get
+            {
+                return GetResourceKey("SeparatorLineBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseOverBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseOverBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseOverTextBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseOverTextBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseDownBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseDownBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseDownTextBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseDownTextBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object GlyphBrushKey
+        {
+            get
+            {
+                return GetResourceKey("GlyphBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseOverGlyphBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseOverGlyphBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object MouseDownGlyphBrushKey
+        {
+            get
+            {
+                return GetResourceKey("MouseDownGlyphBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object DefaultBrushKey
+        {
+            get
+            {
+                return GetResourceKey("DefaultBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object DefaultTextBrushKey
+        {
+            get
+            {
+                return GetResourceKey("DefaultTextBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        private static object GetResourceKey(string resourceName, object defaultKey)
+        {
+            return PlatformColorHelper.GetResourceKey(typeof(HeaderColors), resourceName) ?? defaultKey;
+        }
+    }
+}
---
+++ PlatformUI/TreeViewColors.cs
@@ -0,0 +1,49 @@
+﻿namespace GitScc.PlatformUI
+{
+    using System.Diagnostics;
+    using Microsoft.VisualStudio.Shell;
+
+    public static class TreeViewColors
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object SelectedItemActiveBrushKey
+        {
+            get
+            {
+                return GetResourceKey("SelectedItemActiveBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object SelectedItemActiveTextBrushKey
+        {
+            get
+            {
+                return GetResourceKey("SelectedItemActiveTextBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object SelectedItemInactiveBrushKey
+        {
+            get
+            {
+                return GetResourceKey("SelectedItemInactiveBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static object SelectedItemInactiveTextBrushKey
+        {
+            get
+            {
+                return GetResourceKey("SelectedItemInactiveTextBrushKey", VsBrushes.AccentBorderKey);
+            }
+        }
+
+        private static object GetResourceKey(string resourceName, object defaultKey)
+        {
+            return PlatformColorHelper.GetResourceKey(typeof(TreeViewColors), resourceName) ?? defaultKey;
+        }
+    }
+}

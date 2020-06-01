8077cf7454bfad1aa77e9060182680b3767fa707
Fredy Marín <fredymarinduarte@gmail.com>
2020-06-01 16:02:52 -0500

    Installer for VS2008

--- MakeMsi.bat
+++ MakeMsi.bat
@@ -1,4 +1,4 @@
 del GitSccProvider.wixobj
 del GitSccProvider.msi
-"C:\Program Files\Microsoft Visual Studio 2008 SDK\VisualStudioIntegration\Tools\Wix\candle" GitSccProvider.wxs
-"C:\Program Files\Microsoft Visual Studio 2008 SDK\VisualStudioIntegration\Tools\Wix\light" GitSccProvider.wixobj
+"%VSSDK90Install%\VisualStudioIntegration\Tools\Wix\candle" GitSccProvider.wxs
+"%VSSDK90Install%\VisualStudioIntegration\Tools\Wix\light" GitSccProvider.wixobj
